ab448bf6fe0d063d56a90a0fd7eeedfd96c4c878
draawn <vasil.yoshev@gmail.com>
2018-05-28 02:33:18 +0300

    Fix login validtion bug

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,6 +42,7 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['']);
       } else {
         this.loginService.loggedIn = false;
+        this.wrongCredentials = true;
       }
     });
   }
