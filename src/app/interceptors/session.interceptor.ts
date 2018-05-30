e6174cbba1f447b322b637794cf88e6c977e29db
draawn <vasil.yoshev@gmail.com>
2018-05-30 16:15:28 +0300

    Fix interceptor for rxjs 6

--- src/app/interceptors/session.interceptor.ts
+++ src/app/interceptors/session.interceptor.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpInterceptor, HttpHandler, HttpEvent, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { tap, map, catchError } from 'rxjs/operators';
 
 import { LoginService } from './../login/login.service';
@@ -18,12 +18,12 @@ export class SessionInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next
             .handle(req).pipe(
-                catchError(response => {
+                catchError((response: HttpEvent<any>) => {
                     if (response instanceof HttpErrorResponse) {
                         this.loginService.loggedIn = false;
                         this.router.navigate(['']); // TODO add session expired screen
                     }
-                    return Observable.throw(response);
+                    return throwError(response);
                 })
             );
     }
