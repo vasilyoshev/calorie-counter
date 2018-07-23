ec9076621507dc0410dd7a05eed7d0103a48c8fb
draawn <vasil.yoshev@gmail.com>
2018-07-23 12:55:31 +0300

    Remove unused onInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { ProfileService } from './../profile/profile.service';
 import { LoginService } from './../login/login.service';
@@ -8,13 +8,10 @@ import { LoginService } from './../login/login.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor(
     public loginService: LoginService,
     public profileService: ProfileService
   ) { }
-
-  ngOnInit() {
-  }
 }
