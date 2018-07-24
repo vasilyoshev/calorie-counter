b9655022d76e65c5d111aa104a1ed04dd9b35fef
draawn <vasil.yoshev@gmail.com>
2018-07-24 19:18:03 +0300

    Make requests retry only if server error 500

--- src/app/interceptors/server-errors.interceptor.ts
+++ src/app/interceptors/server-errors.interceptor.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { retry } from '../../../node_modules/rxjs/internal/operators/retry';
+import { Observable, throwError, of } from 'rxjs';
+import { retryWhen, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class ServerErrorsInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req).pipe(retry(2));
+        const maxRetryAttempts = 5;
+        const targetStatusCode = 500;
+        return next.handle(req).pipe(retryWhen((errors) => {
+            return errors.pipe(mergeMap((error, i) => {
+                const retryAttempt = i + 1;
+                if (retryAttempt > maxRetryAttempts || error.status !== targetStatusCode) {
+                    return throwError(error);
+                }
+                return of(true);
+            }));
+        }));
     }
 }
