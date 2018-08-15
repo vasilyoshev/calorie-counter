488e76a0512bf4c41ed768203a0a3a5e5223d975
draawn <vasil.yoshev@gmail.com>
2018-08-15 17:33:51 +0300

    Fix bug for pending requests with auth

--- server/config/auth-middleware.js
+++ server/config/auth-middleware.js
@@ -1,11 +1,13 @@
 /**
  * Checks if user is logged in, by checking if user is stored in session.
  */
-module.exports = authMiddleware = (req, res) => {
+module.exports = authMiddleware = (req, res, next) => {
     if (!req.session || !req.session.username) {
         res.status(403).json({
             success: false,
             message: 'You must be logged in.'
         });
+    } else {
+        next();
     }
 };
