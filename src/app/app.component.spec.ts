41664e7afdee64660a0f509b44f2a8c432e1fa57
draawn <vasil.yoshev@gmail.com>
2018-08-23 20:01:03 +0300

    Replace karma and jasmine with jest and test app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,78 @@
-import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material';
+
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { of } from 'rxjs';
+
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginService } from './login/login.service';
+import { ProfileService } from './profile/profile.service';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ToolbarComponent
       ],
+      imports: [
+        RouterTestingModule,
+        NgxSpinnerModule,
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        HttpClientModule
+      ],
+      providers: [
+        LoginService,
+        ProfileService,
+        NgxSpinnerService
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  });
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
   }));
+  it('should get profile and close spinner when logged in', () => {
+    // GIVEN
+    jest.spyOn(TestBed.get(LoginService), 'isLoggedIn')
+      .mockImplementation(() => of(true));
+
+    const profileServiceSpy = jest.spyOn(TestBed.get(ProfileService), 'getProfile')
+      .mockImplementation(() => of());
+
+    const spinnerSpy = jest.spyOn(TestBed.get(NgxSpinnerService), 'hide');
+    // WHEN
+    component.checkIfLoggedIn();
+
+    // THEN
+    expect(profileServiceSpy).toHaveBeenCalledTimes(1);
+    expect(spinnerSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should get close spinner when NOT logged in', () => {
+    // GIVEN
+    jest.spyOn(TestBed.get(LoginService), 'isLoggedIn')
+      .mockImplementation(() => of(false));
+
+    const spinnerSpy = jest.spyOn(TestBed.get(NgxSpinnerService), 'hide');
+    // WHEN
+    component.checkIfLoggedIn();
+
+    // THEN
+    expect(spinnerSpy).toHaveBeenCalledTimes(1);
+  });
 });
