d53aec2b4ba5e1a8c571188a4cee71a0c9d10b54
draawn <vasil.yoshev@gmail.com>
2018-08-24 10:36:55 +0300

    Set up home component spec

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  MatCardModule, MatIconModule, MatExpansionModule, MatSliderModule, MatDividerModule,
+  MatStepperModule, MatDatepickerModule, MatTableModule, MatInputModule
+} from '@angular/material';
 
 import { HomeComponent } from './home.component';
+import { DiaryComponent } from './diary/diary.component';
+import { AddGoalComponent } from '../add-goal/add-goal.component';
+import { MacrosTableComponent } from './diary/macros-table/macros-table.component';
+import { SearchComponent } from './../search/search.component';
+import { CalendarComponent } from './../shared/calendar/calendar.component';
+import { LoginService } from './../login/login.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +21,33 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent,
+        DiaryComponent,
+        AddGoalComponent,
+        CalendarComponent,
+        SearchComponent,
+        MacrosTableComponent
+      ],
+      imports: [
+        ReactiveFormsModule,
+        MatCardModule,
+        MatIconModule,
+        MatExpansionModule,
+        MatSliderModule,
+        MatDividerModule,
+        MatStepperModule,
+        MatInputModule,
+        RouterTestingModule,
+        HttpClientModule,
+        MatDatepickerModule,
+        MatTableModule
+      ],
+      providers: [
+        LoginService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
