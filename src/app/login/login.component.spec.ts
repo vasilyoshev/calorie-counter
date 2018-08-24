b068fc37f24f90de6882f15bc34b10110d6514ed
draawn <vasil.yoshev@gmail.com>
2018-08-24 10:53:27 +0300

    Remove unused import

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatCardModule, MatCheckboxModule, MatInputModule } from '@angular/material';
 
 import { LoginComponent } from './login.component';
 import { LoginService } from './login.service';
@@ -19,7 +19,6 @@ describe('LoginComponent', () => {
         ReactiveFormsModule,
         MatCardModule,
         MatCheckboxModule,
-        MatFormFieldModule, // TODO
         RouterTestingModule,
         HttpClientModule,
         MatInputModule,
