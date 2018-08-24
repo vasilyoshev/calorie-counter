412116787b8f49a6ad39b59d8e3a9aa3b4e4e7e4
draawn <vasil.yoshev@gmail.com>
2018-08-24 11:14:05 +0300

    Set up spec

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatSnackBarModule, MatInputModule } from '@angular/material';
 
 import { RegisterComponent } from './register.component';
+import { RegisterService } from './register.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +14,21 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [RegisterComponent],
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatCardModule,
+        MatSnackBarModule,
+        MatInputModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        RegisterService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
