e8abfdaa6fb565eb4cc9196baba8ecf6635c017e
draawn <vasil.yoshev@gmail.com>
2018-08-24 10:20:42 +0300

    Set up macros table spec

--- src/app/home/diary/macros-table/macros-table.component.spec.ts
+++ src/app/home/diary/macros-table/macros-table.component.spec.ts
@@ -1,3 +1,4 @@
+import { MatTableModule } from '@angular/material';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MacrosTableComponent } from './macros-table.component';
@@ -8,7 +9,10 @@ describe('MacrosTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MacrosTableComponent ]
+      declarations: [ MacrosTableComponent ],
+      imports: [
+        MatTableModule
+      ]
     })
     .compileComponents();
   }));
