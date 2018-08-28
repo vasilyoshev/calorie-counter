ae113ad7af40b2f01bc45af46793eca081e922cd
draawn <vasil.yoshev@gmail.com>
2018-08-28 21:13:30 +0300

    add tests

--- src/app/home/diary/macros-table/macros-table.component.spec.ts
+++ src/app/home/diary/macros-table/macros-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { MatTableModule } from '@angular/material';
+import { MatTableModule, MatTableDataSource } from '@angular/material';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MacrosTableComponent } from './macros-table.component';
@@ -9,12 +9,12 @@ describe('MacrosTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MacrosTableComponent ],
+      declarations: [MacrosTableComponent],
       imports: [
         MatTableModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,5 +25,32 @@ describe('MacrosTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize correctly without quantity', () => {
+    expect(component.displayedColumns).toEqual(['name', 'calories', 'protein', 'carbs', 'fat']);
+  });
+
+  it('should initialize correctly with quantity', () => {
+    // GIVEN
+    component.hasQuantity = true;
+
+    // WHEN
+    component.ngOnInit();
+
+    // THEN
+    expect(component.displayedColumns).toEqual(['name', 'quantity', 'calories', 'protein', 'carbs', 'fat']);
+  });
+
+  it('should set dataSource data correctly', () => {
+    // GIVEN
+    component.data = [{ name: 'test', quantity: 100, calories: 100, protein: 10, carbs: 10, fat: 10 }];
+    component.dataSource = new MatTableDataSource();
+
+    // WHEN
+    component.ngOnChanges();
+
+    // THEN
+    expect(component.dataSource.data).toEqual(component.data);
   });
 });
