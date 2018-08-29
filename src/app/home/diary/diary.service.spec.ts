dc4c0b5ce5ce7199515ce069aec839d62c2e70cd
draawn <vasil.yoshev@gmail.com>
2018-08-29 11:24:00 +0300

    add tests

--- src/app/home/diary/diary.service.spec.ts
+++ src/app/home/diary/diary.service.spec.ts
@@ -1,7 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { DiaryService } from './diary.service';
+import { of } from 'rxjs';
 
 describe('DiaryService', () => {
   beforeEach(() => {
@@ -13,5 +14,24 @@ describe('DiaryService', () => {
 
   it('should be created', inject([DiaryService], (service: DiaryService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should set service properties accordingly', inject([DiaryService], (service: DiaryService) => {
+    // GIVEN
+    const mealDate = '2018-08-27T15:18:54.734Z';
+    const postDaySpy = jest.spyOn(TestBed.get(HttpClient), 'post')
+      .mockImplementation(() => of({summary: [], meals: [{date: mealDate}]}));
+
+    // WHEN
+    service.getDay(new Date('2018-08-26T21:00:00.000Z')).subscribe();
+
+    // THEN
+    expect(postDaySpy).toHaveBeenCalled();
+    expect(service.summary).toEqual([]);
+    expect(service.meals[0].date).toEqual(mealDate);
+    expect(service.meals[0].time).toEqual('18:18'); // local time GMT+3
+    expect(service.currentDate.getDate()).toEqual(27);
+    expect(service.currentDate.getMonth()).toEqual(7);
+    expect(service.currentDate.getFullYear()).toEqual(2018);
   }));
 });
